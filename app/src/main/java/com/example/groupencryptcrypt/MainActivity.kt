da7731d37136e17209d09752fac7de696c3e0aed
Luis Ortiz <luchoaam97@gmail.com>
2023-08-20 18:17:43 -0500

    first commit

---
+++ app/src/main/java/com/example/groupencryptcrypt/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.example.groupencryptcrypt
+
+import android.accounts.AccountManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
+import android.widget.TextView
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
+import com.example.groupencryptcrypt.databinding.ActivityMainBinding
+import java.io.IOException
+import java.nio.charset.StandardCharsets
+import java.security.GeneralSecurityException
+import java.security.KeyStore
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.encryptButton.setOnClickListener {
+            val dataToEncrypt = binding.inputEditText.text.toString()
+
+            val encryptedData = encryptData(dataToEncrypt)
+            binding.encryptedTextView.text = "Encrypted Text: ${encryptedData.contentToString()}"
+
+            val decryptedData = decryptData(encryptedData)
+            binding.decryptedTextView.text = "Decrypted Text: $decryptedData"
+
+            val masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+            binding.masterKeyTextView.text = "Master Key Alias: $masterKeyAlias"
+
+            binding.inputEditText.text?.clear()
+        }
+    }
+
+    private fun encryptData(data: String): ByteArray {
+        val masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+        val encryptedSharedPreferences = EncryptedSharedPreferences.create(
+            "encrypted_prefs",
+            masterKeyAlias,
+            this,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+
+        encryptedSharedPreferences.edit()
+            .putString("encrypted_data", data)
+            .apply()
+
+        return data.toByteArray(StandardCharsets.UTF_8)
+    }
+
+    private fun decryptData(encryptedData: ByteArray): String {
+        val masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+        val encryptedSharedPreferences = EncryptedSharedPreferences.create(
+            "encrypted_prefs",
+            masterKeyAlias,
+            this,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+
+        val decryptedData = encryptedSharedPreferences.getString("encrypted_data", null)
+        return decryptedData ?: "Decryption failed"
+    }
+
+    private fun getOrCreateSecretKey(masterKeyAlias: String): SecretKey {
+        val keyStore = KeyStore.getInstance("AndroidKeyStore")
+        keyStore.load(null)
+
+        if (!keyStore.containsAlias(masterKeyAlias)) {
+            val keyGenerator = KeyGenerator.getInstance(
+                KeyProperties.KEY_ALGORITHM_AES,
+                "AndroidKeyStore"
+            )
+            val keyGenParameterSpec = KeyGenParameterSpec.Builder(
+                masterKeyAlias,
+                KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+            )
+                .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+                .build()
+            keyGenerator.init(keyGenParameterSpec)
+            keyGenerator.generateKey()
+        }
+
+        return keyStore.getKey(masterKeyAlias, null) as SecretKey
+    }
+
+}
